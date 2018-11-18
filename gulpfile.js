d0b00fec2f3ffd41644859cd14c4e8cbc1a46063
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-11-19 00:00:00 +0100

    Fixed sync in gulp and make it similar to other gulp files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,38 @@
 
 const del = require('del');
 const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const lec = require('gulp-line-ending-corrector');
 const filter = require('gulp-filter');
 
-const sourceDir = 'node_modules/epubjs-reader/reader/**';
-const destinationDir = 'asset/vendor/epubjs-reader';
+const bundle = [
+    {
+        'source': 'node_modules/epubjs-reader/reader/**',
+        'dest': 'asset/vendor/epubjs-reader',
+    },
+];
 
 gulp.task('clean', function(done) {
-    return del(destinationDir);
+    bundle.forEach(function (module) {
+        return del.sync(module.dest);
+    });
+    done();
 });
 
-gulp.task('sync', function (next) {
-    const f = filter(['**/*.html', '**/*.js', '**/*.css'], {restore: true});
-    gulp.src([sourceDir])
-    .pipe(f)
-    .pipe(lec())
-    .pipe(f.restore)
-    .pipe(gulp.dest(destinationDir))
-    .on('end', next);
+gulp.task('sync', function (done) {
+    const filtering = filter(['**/*.html', '**/*.js', '**/*.css'], {restore: true});
+    bundle.forEach(function (module) {
+        gulp.src(module.source)
+            .pipe(filtering)
+            .pipe(lec())
+            .pipe(filtering.restore)
+            .pipe(gulpif(module.rename, rename({suffix:'.min'})))
+            .pipe(gulpif(module.uglify, uglify()))
+            .pipe(gulp.dest(module.dest));
+    });
+    done();
 });
 
 gulp.task('default', gulp.series('clean', 'sync'));
